9623f8651077bddc41ed04c072b4e984f5f35002
Brandon Hofman <brandon.hoffman@climate.com>
2022-02-15 16:13:59 -0800

    completed problem

--- 01_sliding_window/07-longest_substring_with_k_distinct.py
+++ 01_sliding_window/07-longest_substring_with_k_distinct.py
@@ -1,42 +1,66 @@
+# Solution is a bit different than official solution. Consider changing
 
 
-
-def smallest_subbarray_with_greater_sum(s, arr):
-    min_arr_len = 0
+def longest_substring_with_k_distinct(str1, k):
 
     window_start = 0
-    window_sum = 0
+    longest_substring = 0
+    char_frequency = dict()
     
+    for window_end in range(len(str1)):
+        latest_char = str1[window_end]
+        
+        if latest_char not in char_frequency:
+            char_frequency[latest_char] = 1
+        else:
+            char_frequency[latest_char] += 1
 
-    for window_end in range(len(arr)):
-        window_sum += arr[window_end]
-        window_len = window_end - window_start + 1
+        if len(char_frequency) <= k:
+            longest_substring = max(longest_substring, window_end - window_start + 1)
 
-    
-        while window_sum >= s:
-            if min_arr_len == 0:
-                min_arr_len = window_len
-            min_arr_len = min(min_arr_len, window_len)
-            window_sum -= arr[window_start]
+        while len(char_frequency) > k:
+            earliest_char = str1[window_start]
+            
+            try:
+                if earliest_char in char_frequency:
+                    char_frequency[earliest_char] -=1
+                
+                if char_frequency[earliest_char] == 0:
+                    char_frequency.pop(earliest_char)
+            except KeyError:
+                pass
+
             window_start += 1
-            window_len = window_end - window_start + 1
+
+    return longest_substring
 
 
-    return min_arr_len
+        
+
+    
 
 def main():
-    s = 7
-    result = smallest_subbarray_with_greater_sum(s, [2, 1, 5, 2, 3, 2])
-    print(f"The smallest continguous subarray with a sum greater than or equal to '{s}'': " + str(result))
+    k = 2
+    result = longest_substring_with_k_distinct("araaci", k)
+    print(f"The smallest continguous subarray with a sum greater than or equal to '{k}'': " + str(result))
+    
+    k = 1
+    result = longest_substring_with_k_distinct("araaci", k)
+    print(f"The smallest continguous subarray with a sum greater than or equal to '{k}'': " + str(result))
 
-    s = 7
-    result = smallest_subbarray_with_greater_sum(s, [2, 1, 5, 2, 8])
-    print(f"The smallest continguous subarray with a sum greater than or equal to '{s}'': " + str(result))
+    k = 3
+    result = longest_substring_with_k_distinct("cbbebi", k)
+    print(f"The smallest continguous subarray with a sum greater than or equal to '{k}'': " + str(result))
 
-    s = 8
-    result = smallest_subbarray_with_greater_sum(s, [3, 4, 1, 1, 6])
-    print(f"The smallest continguous subarray with a sum greater than or equal to '{s}'': " + str(result))
+    k = 10
+    result = longest_substring_with_k_distinct("cbbebi", k)
+    print(f"The smallest continguous subarray with a sum greater than or equal to '{k}'': " + str(result))
 
+    # Time Complexity will be O(N). The outer for loop runs for all character, and the inner while loop processes
+    # each character only once; therefore, the time complexity of the algorithm will be O(N + N), which is asymptotically
+    # equivalent to O(N)
+
+    # Space Complexity is O(K), as we will be storing a maximum of K+1 characters in the HashMap
 
 main()
 
