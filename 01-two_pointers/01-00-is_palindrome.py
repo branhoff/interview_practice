8bb192af26f9ac49d27ded3756e3185bbe4dd19b
Brandon Hoffman <branhoff2@gmail.com>
2023-11-01 22:17:18 -0700

    restructuring based on material as of Oct 2023

---
+++ 01-two_pointers/01-00-is_palindrome.py
@@ -0,0 +1,39 @@
+def is_palindrome(s):
+
+  head = 0
+  tail = len(s) - 1
+  i = 0
+
+  while i != len(s) // 2:
+    if s[head] != s[tail]:
+      return False
+    else:
+      head += 1
+      tail -= 1
+      i += 1
+
+  return True
+
+def solution(array):
+    left = 0
+    right = len(array) - 1
+
+    while left <= right:
+        if s[left] != s[right]:
+            return False
+        else:
+            left = left + 1
+            right = right -1
+
+    return True
+
+
+def main():
+    print(is_palindrome("kayak"))
+    print(is_palindrome("hello"))
+    print(is_palindrome("RACEACAR"))
+    print(is_palindrome("A"))
+    print(is_palindrome("ABCDABCD"))
+
+if __name__ == "__main__":
+    main()
