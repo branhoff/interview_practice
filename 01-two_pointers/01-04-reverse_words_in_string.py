a6b2ca4478b0c89886ec6c9dc9e102e61a68dad1
Brandon Hoffman <branhoff2@gmail.com>
2023-11-13 22:47:40 -0800

    official grokking solution

--- 01-two_pointers/01-04-reverse_words_in_string.py
+++ 01-two_pointers/01-04-reverse_words_in_string.py
@@ -1,23 +1,51 @@
+import re
 
 def reverse_words(sentence):
-    reversed_sentence = sentence[::-1]
-    output = ""
+   sentence = re.sub(' +',' ',sentence.strip())
 
-    head = 0
+   sentence = list(sentence)
+   str_len = len(sentence)
 
-    for i in range(len(reversed_sentence)):
-        if reversed_sentence[i] == " " or i == len(reversed_sentence) - 1:
-            if i == len(reversed_sentence) - 1:
-                output += reversed_sentence[i:head-1:-1]
-            else:
-                output += reversed_sentence[i:head:-1] + " "
+   str_rev(sentence, 0, str_len - 1)
 
-            head = i + 1
+   start = 0
+   end = 0
 
-    return output.strip()
+   while (start < str_len):
+    while end < str_len and sentence[end] != ' ':
+        end += 1
+    str_rev(sentence, start, end - 1)
+    start = end + 1;
+    end += 1
 
+   return ''.join(sentence)
+
+
+# A function that reverses a whole sentence character by character
+def str_rev(_str, start_rev, end_rev):
+   while start_rev < end_rev:
+       temp = _str[start_rev]
+       _str[start_rev] = _str[end_rev]
+       _str[end_rev] = temp
+
+       start_rev += 1
+       end_rev -= 1
+
+# Driver code
 def main():
-    print(reverse_words("We love Java "))
+    string_to_reverse = ["Hello Friend", "    We love Python",
+                         "The quick brown fox jumped over the lazy dog   ",
+                         "Hey", "To be or not to be",
+                         "AAAAA","Hello     World"]
 
-if __name__ == "__main__":
+    for i in range(len(string_to_reverse)):
+        print(i+1, ".\t Actual string:\t\t" +
+              "".join(string_to_reverse[i]), sep='')
+        Result = reverse_words(string_to_reverse[i])
+        print("\t Reversed string:\t" +
+              "".join(Result), sep='')
+        print("-"*100)
+
+
+if __name__ == '__main__':
     main()
